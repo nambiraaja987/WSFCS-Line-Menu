7aa44227cecfdb83c61a70b2eaa2db73a33ccf1c
Nambiraaja <87847173+nambiraaja987@users.noreply.github.com>
2026-01-07 14:25:36 -0500

    Update test.py

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ from docx.enum.text import WD_ALIGN_PARAGRAPH, WD_BREAK
 st.set_page_config(page_title="WSFCS Menu Generator", layout="wide")
 
 # --- FILE PATHS ( HARDCODED FOR AUTOMATION ) ---
-BASE_DIR = "BASE_URL = "https://raw.githubusercontent.com/hamibraaja987/WSFCS-Line-Menu/main"
+BASE_DIR = "https://raw.githubusercontent.com/hamibraaja987/WSFCS-Line-Menu/main"
 CSV_PATH = os.path.join(BASE_DIR, "Schools.csv")
 WSFCS_LOGO = os.path.join(BASE_DIR, "wsfcs.png")
 CHARTWELLS_LOGO = os.path.join(BASE_DIR, "Chartwells.png")
@@ -283,5 +283,6 @@ if st.button("🚀 Generate Menus", type="primary"):
         mime="application/zip"
 
     )
+
 
 
