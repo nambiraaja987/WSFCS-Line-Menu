390058be4d312b7e02fa35bda1ac4e44c0b8f172
Nambiraaja <87847173+nambiraaja987@users.noreply.github.com>
2026-01-07 17:13:50 -0500

    Update test.py

--- test.py
+++ test.py
@@ -367,5 +367,6 @@ if os.path.exists(CHARTWELLS_LOGO_FILENAME):
     # Use columns to push logo to the right
     fc1, fc2 = st.columns([5, 1]) 
     with fc2:
-        st.image(CHARTWELLS_LOGO_FILENAME, width=120)
+        st.image(CHARTWELLS_LOGO_FILENAME, width=220)
+
 
